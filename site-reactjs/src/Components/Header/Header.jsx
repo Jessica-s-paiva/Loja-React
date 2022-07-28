5e88bba044a777cecba87c4efe744587035b3690
Jessica-s-paiva <jessicapaivamvp@gmail.com>
2022-07-28 20:02:15 -0300

    feat: adicionando links no header

--- site-reactjs/src/Components/Header/Header.jsx
+++ site-reactjs/src/Components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React, { StrictMode } from 'react';
 import S from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header = ({titulo}) => {
   return (
     <header className={S.container}>
         <h4>{titulo}</h4>
         <nav>
-          
+          <Link className={S.link} to='/'>Home</Link>
+          <Link className={S.link} to='/cadastro'>Cadastro</Link>
         </nav>
     </header>
   )
